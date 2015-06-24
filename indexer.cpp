ced2301d45ea184b7ce12860fa448bb782bf9f2c
Max Lam <malam@linkedin.com>
2015-06-23 21:07:35 -0700

    Fixed indexer bugs

--- indexer.cpp
+++ indexer.cpp
@@ -1,11 +1,35 @@
+#include <iostream>
 #include <fstream>
 #include <map>
 #include <vector>
 #include <dirent.h>
+#include <iostream>
+#include <unistd.h>
+#include <string>
 
 using namespace std;
 
 map<string, vector<string> > inv_index;
+const char * extensions[] = {"txt", "doc"};
+vector<string> valid_extensions(extensions, end(extensions));
+
+bool is_valid_file(string name) {
+    string ext = name.substr(name.find_last_of(".")+1);
+    for (int i = 0; i < valid_extensions.size(); i++) {
+        if (ext == valid_extensions[i]) return true;
+    }
+    return false;
+}
+
+bool is_valid_dir(string name) {
+    return name[0] != '.';
+}
+
+string get_cwd() {
+    char cur_path[FILENAME_MAX];
+    getcwd(cur_path, sizeof(cur_path));
+    return string(cur_path);
+}
 
 bool is_alphanum(char c) {
     if ((('a' <= c) && (c <= 'z')) || (('0'<= c) && (c <= '9')) ||  (('A'<= c) && (c <= 'Z'))) {
@@ -21,9 +45,10 @@ void index_file(string file_path) {
         in >> s;
         std::transform(s.begin(), s.end(), s.begin(), ::tolower);
         s.erase(std::remove_if(s.begin(), s.end(), is_alphanum), s.end());
-        if (inv_index.count(s) != 0) {
+        if (s.size() == 0) continue;
+        if (inv_index.count(s) == 0) {
             vector<string> v;
-            v.push_back(s);
+            v.push_back(file_path);
             inv_index[s] = v;
         } else {
             vector<string> v = inv_index[s];
@@ -47,9 +72,9 @@ void list_directory_contents(string path, vector<string>& dirs, vector<string>& 
     }
 
     while ((entry = readdir(dp))) {
-        if (entry->d_type == DT_DIR) {
-            dirs.push_back(string(entry->d_name)); 
-        } else if (entry->d_type == DT_REG) {
+        if (entry->d_type == DT_DIR && is_valid_dir(entry->d_name)) {
+            dirs.push_back(string(entry->d_name));
+        } else if (entry->d_type == DT_REG && is_valid_file(entry->d_name)) {
             files.push_back(string(entry->d_name));
         }
     }
@@ -62,7 +87,7 @@ void get_all_files(string base_path, vector<string> &all_files) {
     vector<string> dirs;
     list_directory_contents(base_path, dirs, all_files);
     for (int i = 0; i < dirs.size(); i++) {
-        get_all_files(dirs[i], all_files);
+        get_all_files(base_path + "/" + dirs[i], all_files);
     }
 }
 
@@ -74,5 +99,14 @@ void index_all_files(string base_path) {
     }
 }
 
-
-
+int main() {
+  index_all_files(get_cwd());
+  for (std::map<string,vector<string> >::iterator it=inv_index.begin(); it!=inv_index.end(); ++it) {
+    cout << it -> first << endl;
+    vector<string> v = it -> second;
+    for (int i=0; i < v.size(); i++) {
+      cout << v[i]<< " ";
+    }
+    cout<<endl;
+  }
+}
