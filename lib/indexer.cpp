fcb9993536d4344d87351a095b457c4faa90b4c7
vmohan@mit.edu <varunkmohan@gmail.com>
2015-06-24 19:39:31 -0700

    Implemented indexer parsing

--- lib/indexer.cpp
+++ lib/indexer.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <utility>
+#include <unordered_set>
 #include <map>
 #include <vector>
 #include <dirent.h>
@@ -15,6 +17,7 @@
 
 using namespace std;
 
+unordered_set<string> stopwords;
 map<string, vector<string> > inv_index;
 const char * extensions[] = {"txt", "doc"};
 vector<string> valid_extensions(extensions, end(extensions));
@@ -33,6 +36,20 @@ void serialize(map<string, vector<string> > map, string file_out) {
    oarch << map;
 }
 
+void read_stopwords(string fname) {
+    ifstream in(fname);
+    while (!in.eof()) {
+        string cur;
+        in >> cur;
+        stopwords.insert(cur);
+    }
+    in.close();
+}
+
+bool is_stopword(string word) {
+    return stopwords.find(word) != stopwords.end();
+}
+
 bool is_valid_file(string name) {
     string ext = name.substr(name.find_last_of(".")+1);
     for (int i = 0; i < valid_extensions.size(); i++) {
@@ -47,33 +64,56 @@ bool is_valid_dir(string name) {
 
 bool is_alphanum(char c) {
     if ((('a' <= c) && (c <= 'z')) || (('0'<= c) && (c <= '9')) ||  (('A'<= c) && (c <= 'Z'))) {
-        return false;
+        return true;
     }
-    return true;
+    return false;
 }
 
-void index_file(string file_path) {
+inline void index_term(string s, string file_path) {
+    if (is_stopword(s)) return;
+    std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+    if (inv_index.count(s) == 0) {
+        vector<string> v;
+        v.push_back(file_path);
+        inv_index[s] = v;
+    } else {
+        vector<string> v = inv_index[s];
+        if (v[v.size() - 1] != file_path) {
+            inv_index[s].push_back(file_path);
+        }
+    }
+}
+
+inline void index_string(string s, string file_path) {
+    std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+    string curstring = "";
+    for (int i=0; i< s.size(); i++) {
+        if (!is_alphanum(s[i])) {
+            if (curstring.size() > 0) {
+                index_term(curstring, file_path);
+            }
+            curstring = "";
+        } else {
+            curstring += s[i];
+        }
+    }
+    if (curstring != "") {
+        index_term(curstring, file_path);
+    }
+}
+
+void index_file(string file_path, string file_name) {
     ifstream in(file_path.c_str());
     while (!in.eof()) {
         string s;
         in >> s;
-        std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-        s.erase(std::remove_if(s.begin(), s.end(), is_alphanum), s.end());
-        if (inv_index.count(s) == 0) {
-            vector<string> v;
-            v.push_back(file_path);
-            inv_index[s] = v;
-        } else {
-            vector<string> v = inv_index[s];
-            if (v[v.size() - 1] != file_path) {
-                inv_index[s].push_back(file_path);
-            }
-        }
+        index_string(s, file_path);
     }
+    index_string(file_name, file_path);
     in.close();
 }
 
-void list_directory_contents(string path, vector<string>& dirs, vector<string>& files) {
+void list_directory_contents(string path, vector<string>& dirs, vector<pair<string, string> >& files) {
     vector<string> results;
     struct dirent *entry;
     DIR *dp;
@@ -88,7 +128,7 @@ void list_directory_contents(string path, vector<string>& dirs, vector<string>& 
         if (entry->d_type == DT_DIR && is_valid_dir(entry->d_name)) {
             dirs.push_back(string(entry->d_name));
         } else if (entry->d_type == DT_REG && is_valid_file(entry->d_name)) {
-            files.push_back(path+"/"+string(entry->d_name));
+            files.push_back(make_pair(path+"/"+string(entry->d_name), string(entry->d_name)));
         }
     }
 
@@ -96,7 +136,7 @@ void list_directory_contents(string path, vector<string>& dirs, vector<string>& 
     return;
 }
 
-void get_all_files(string base_path, vector<string> &all_files) {
+void get_all_files(string base_path, vector<pair<string, string> > &all_files) {
     vector<string> dirs;
     list_directory_contents(base_path, dirs, all_files);
     for (int i = 0; i < dirs.size(); i++) {
@@ -105,10 +145,11 @@ void get_all_files(string base_path, vector<string> &all_files) {
 }
 
 void index_all_files(string base_path) {
-    vector<string> all_files;
+    read_stopwords("lib/stopwords.txt");
+    vector<pair<string, string> > all_files;
     get_all_files(base_path, all_files);
     for (int i = 0; i < all_files.size(); i++) {
-        index_file(all_files[i]);
+        index_file(all_files[i].first, all_files[i].second);
     }
 }
 
@@ -116,20 +157,28 @@ void test_serialization() {
     serialize(inv_index, "test-serialization");
     map<string, vector<string> > deserialized = deserialize("test-serialization");
     for (std::map<string,vector<string> >::iterator it=inv_index.begin(); it!=inv_index.end(); ++it) {
-	vector<string> v1 = it->second;
-	vector<string> v2 = deserialized[it->first];
-    	for (int i = 0; i < v1.size(); i++) {
-    	    if (v1[i] != v2[i]) {
-    		perror("Bad serialization...");
-    		exit(0);
-    	    }
-    	}
+        vector<string> v1 = it->second;
+        vector<string> v2 = deserialized[it->first];
+        for (int i = 0; i < v1.size(); i++) {
+            if (v1[i] != v2[i]) {
+            perror("Bad serialization...");
+            exit(0);
+            }
+        }
     }
-    cout << "SERIALIZATION SUCCESS" << endl;
 }
 
 int main() {
     index_all_files(".");
-    cout << "INDEX BUILD SUCCESS" << endl;
+    for (std::map<string,vector<string> >::iterator it=inv_index.begin(); it!=inv_index.end(); ++it) {
+    cout << it -> first << endl;
+    vector<string> v = it -> second;
+    for (int i=0; i < v.size(); i++) {
+        cout << v[i]<< " ";
+    }
+    cout<<endl;
+    }
     test_serialization();
 }
+
+
