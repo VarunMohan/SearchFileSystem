9dfd93ea2324d863304db9474e63cfdd4abd5aca
Max Lam <malam@linkedin.com>
2015-06-25 22:15:07 -0700

    Update IndexTokenizer.cpp and add SFSToken.h. Need to resolve paths, etc

--- lib/indexing/tokenizer/IndexTokenizer.cpp
+++ lib/indexing/tokenizer/IndexTokenizer.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <unordered_set>
 #include "SFSToken.h"
 
 using namespace std;
 
 class IndexTokenizer {
 private:
+    unordered_set<string> stopwords;
     ifstream fileInput;
+
+    void readStopWords() {
+	ifstream stopwordsFile("../stopwords.txt");
+	while (!stopwordsFile.eof()) {
+	    string cur;
+	    stopwordsFile >> cur;
+	    this->stopwords.insert(cur);
+	}
+	stopwordsFile.close();
+    }
+
+    //Assumes the current file is at an alphanum char
+    SFSToken nextTokenHelper() {
+	string token = "";
+	int position = this->fileInput.tellg();
+	char c;
+
+	while (is_alphanum(this->fileInput.peek())) {
+	    this->fileInput >> c;
+	    token += c;
+	}
+	this->advanceToAlphanumChar();
+	return (SFSToken){token, position};
+    }
 
     bool is_alphanum(char c) {
 	return ((('a' <= c) && (c <= 'z')) ||
@@ -26,21 +52,16 @@ public:
     //Takes a filename and initializes the IndexTokenizer
     IndexTokenizer(string fname) {
 	this->fileInput.open(fname);
+	this->readStopWords();
 	this->advanceToAlphanumChar();
     }
 
     //Assumes that the first character is alphanum
     SFSToken nextToken() {
-	string token = "";
-	int position = this->fileInput.tellg();
-	char c;
-
-	while (is_alphanum(this->fileInput.peek())) {
-	    this->fileInput >> c;
-	    token += c;
-	}
-	this->advanceToAlphanumChar();
-	return (SFSToken){token, position};
+	SFSToken curToken = this->nextTokenHelper();
+	while (stopwords.find(curToken.tok) != stopwords.end())
+	    curToken = this->nextTokenHelper();
+	return curToken;
     }
 
     bool isEmpty() {
---
+++ lib/indexing/tokenizer/SFSToken.h
@@ -0,0 +1,13 @@
+#ifndef SFSTOKEN
+#define SFSTOKEN
+
+#include <iostream>
+
+using namespace std;
+
+typedef struct sfstoken {
+    string tok;
+    int pos;
+} SFSToken;
+
+#endif
