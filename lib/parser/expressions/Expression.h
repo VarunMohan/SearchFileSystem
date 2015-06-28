ac5bbf169ff5cb52b5b5a5aaaad3569e65782d96
vmohan@mit.edu <varunkmohan@gmail.com>
2015-06-28 16:17:23 -0700

    Added Interface Method

--- lib/parser/expressions/Expression.h
+++ lib/parser/expressions/Expression.h
@@ -2,6 +2,8 @@
 #define EXPRESSION
 
 #include <iostream>
+#include "../datastructures/DocIterator.h"
+
 
 using namespace std;
 
@@ -13,6 +15,7 @@ class Expression {
 
     virtual string toString() = 0;
     virtual void free() = 0;
+    virtual DocIterator getIterator() = 0;
 };
 
 #endif
